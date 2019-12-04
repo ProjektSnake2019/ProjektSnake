e94316acb5506916dc071db4935924489af68cdd
Matrer <koziol.marcin96@gmail.com>
2019-12-04 22:39:17 +0100

    coraz lepiej i interfejs dobrze działa

--- Assets/Snake/SnakeMovement.cs
+++ Assets/Snake/SnakeMovement.cs
@@ -37,10 +37,16 @@ public class SnakeMovement : MonoBehaviour
     private void moveForward() 
     {
         
-        if(transform.rotation.z > 0.8 || transform.rotation.z < -0.8)
+        if(transform.rotation.z > 0.5 || transform.rotation.z < -0.5)
         {
             Debug.Log(transform.rotation.z);
-            transform.rotation = Quaternion.Euler(transform.rotation.x,transform.rotation.y,transform.rotation.z);
+            transform.rotation = Quaternion.Euler(transform.rotation.x,transform.rotation.y,0);
+        }
+
+        if (transform.rotation.x > 0.5 || transform.rotation.x < -0.5)
+        {
+            Debug.Log(transform.rotation.x);
+            transform.rotation = Quaternion.Euler(0, transform.rotation.y, transform.rotation.z);
         }
         innerSpeed = targetSpeed * (transform.localRotation.x/ slowdownAngle);
         if(-0.2f > innerSpeed)
